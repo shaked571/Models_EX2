ca4d44297340aad2e239fc15fb520a5e2108fc9a
danity <danity251@gmail.com>
2020-12-01 16:55:54 +0200

    Corrections for VOCAB_SIZE and perplexity

--- ex2.py
+++ ex2.py
@@ -2,7 +2,9 @@
 import sys
 import math
 from collections import defaultdict
+
 DEBUG = False
+VOCAB_SIZE = 300000
 
 
 def printify(num_output, answer):
@@ -13,34 +15,37 @@ def clean_lines(lines):
     return lines[2::4]
 
 
-def perplexity_lidstone(set_stat: defaultdict, lamb):
+def perplexity_lidstone(set_stat: defaultdict, train_stat: defaultdict, lamb, num_all_events):
     """
     Calculate 2^ -(sum(p(w \in W)/ |H|) when using lidstone to calculate probabilities
     :param set_stat: The set evaluating on
     :param lamb: the lambda value to use in lidstone
+    :param train_stat: data of the trained model
+    :param num_all_events: number of events in train
     :return: the perplexity of the model
     """
     log_sum = 0
     for w in set_stat.keys():
-        log_sum += log2(lidstone(w, set_stat, lamb))
-    prep_score = 2 ** (-(log_sum / len(set_stat)))
-    return prep_score
+        log_sum += log2(lidstone(w, train_stat, lamb, num_all_events)) * set_stat[w]
+    prep_score = 2 ** (-(log_sum / total_event_num(set_stat)))
+    return round(prep_score, 2)
 
 
-def perplexity_held_out(h_stat, h_size, test_stat):
+def perplexity_held_out(h_stat, h_size, test_stat, train_stat):
     """
     Calculate 2^ -(sum(p(w \in W)/ |H|) when using held-out to calculate probabilities
     :param h_stat: H word count
     :param h_size: how many "events" there is in H
     :param test_stat: the test word count
+    :param train_stat: the train word count
     :return: the perplexity of the model
     """
-    t_reverse_stat = create_rev_stat(test_stat)
+    t_reverse_stat = create_rev_stat(train_stat)
     log_sum = 0
     for w in test_stat.keys():
-        log_sum += log2(held_out_estimation(w, h_stat, h_size, test_stat, t_reverse_stat))
-    prep_score = 2 ** (-(log_sum / len(test_stat)))
-    return prep_score
+        log_sum += log2(held_out_estimation(w, h_stat, h_size, train_stat, t_reverse_stat)) * test_stat[w]
+    prep_score = 2 ** (-(log_sum / total_event_num(test_stat)))
+    return round(prep_score, 2)
 
 
 def log2(n):
@@ -79,30 +84,25 @@ def vocab_stt(corpus: list) -> defaultdict:
 
 
 def total_event_num(corpus: defaultdict) -> int:
-    total = 0
-    for s in corpus.values():
-        total += s
-    return total
+    return sum(list(corpus.values()))
 
 
-def lidstone(word, set_stat: defaultdict, lamb: float) -> float:
+def lidstone(word, set_stat: defaultdict, lamb: float, num_all_events) -> float:
     """
     Calculate lidstone estimation using a given lambda -> (c(x) + lambda) / (|S| + lambda|X|)
     :param word: the word to Calculate lidstone estimation on
     :param set_stat: the set stat
     :param lamb: lambda
+    :param num_all_events: size of all set
     :return: the lidstone estimation for the word
     """
-    all_events = total_event_num(set_stat)
     word_events = set_stat[word]
-    vocab_size = len(set_stat)
-    return (word_events + lamb) / (all_events + (lamb * vocab_size))
+    return (word_events + lamb) / (num_all_events + (lamb * VOCAB_SIZE))
 
 
 def init_res(dev_f, input_word, output_f, test_f):
-    vocab_size = 300000
-    o5 = vocab_size
-    o6 = 1 / vocab_size
+    o5 = VOCAB_SIZE
+    o6 = 1 / VOCAB_SIZE
     return ["#Students	Refael Greenfeld	Danit Yshaayahu 305030868	312434269\n",
             printify(1, dev_f),
             printify(2, test_f),
@@ -116,12 +116,26 @@ def part2(dev_stat, res):
     o7 = total_event_num(dev_stat)
     res.append(printify(7, o7))
 
+def find_best_lambda(validation_stat, training_stat, num_all_events):
+    cur_lambda = 0.01
+    min_lambda = 0
+    min_perplexity = float("inf")
+
+    while cur_lambda <= 2:
+        cur_per = perplexity_lidstone(validation_stat, training_stat, cur_lambda, num_all_events)
+        if cur_per < min_perplexity:
+            min_perplexity = cur_per
+            min_lambda = cur_lambda
+        cur_lambda += 0.01
+
+    return round(min_lambda, 2), min_perplexity
 
 def part3(dev, input_word, res):
     training_set = dev[:int(0.9 * len(dev))]
     training_stat = vocab_stt(training_set)
     validation_set = dev[int(0.9 * len(dev)):]
     validation_stat = vocab_stt(validation_set)
+    num_all_events = total_event_num(training_stat)
 
     # Calculate the number of events in the sets
     o8 = total_event_num(validation_stat)
@@ -146,49 +160,48 @@ def part3(dev, input_word, res):
     o13 = training_stat["unseen-word"]
     res.append(printify(13, o13))
 
-    # Lidstone estimation  for lambda 0.1
-    o14 = lidstone(input_word, training_stat, 0.1)
-    o15 = lidstone("unseen-word", training_stat, 0.1)
+    # Lidstone estimation for lambda 0.1
+    o14 = lidstone(input_word, training_stat, 0.1, num_all_events)
+    o15 = lidstone("unseen-word", training_stat, 0.1, num_all_events)
     res.append(printify(14, o14))
     res.append(printify(15, o15))
 
     # Finding the lambda which minimise the perplexity
-    lambda_option = [0.01, 0.1, 1]
-    o16 = perplexity_lidstone(validation_stat, lambda_option[0])
-    o17 = perplexity_lidstone(validation_stat, lambda_option[1])
-    o18 = perplexity_lidstone(validation_stat, lambda_option[2])
+    o16 = perplexity_lidstone(validation_stat, training_stat, 0.01, num_all_events)
+    o17 = perplexity_lidstone(validation_stat, training_stat, 0.1, num_all_events)
+    o18 = perplexity_lidstone(validation_stat, training_stat, 1, num_all_events)
     res.append(printify(16, o16))
     res.append(printify(17, o17))
     res.append(printify(18, o18))
 
-    min_tup = min([(o16, lambda_option[0]), (o17, lambda_option[1]), (o18, lambda_option[2])], key=lambda x: x[0])
-    best_lambda = min_tup[1]
+    best_lambda, best_per = find_best_lambda(validation_stat, training_stat, num_all_events)
+
     o19 = best_lambda
     res.append(printify(19, o19))
-    o20 = min_tup[0]
+    o20 = best_per
     res.append(printify(20, o20))
 
     if DEBUG:
-        sum_lidstone(o19, training_stat, validation_stat)
-    return best_lambda
+        sum_lidstone(o19, training_stat, num_all_events)
+
+    return best_lambda, training_stat, num_all_events
 
 
-def sum_lidstone(lambda_v, training_stat, validation_stat):
+def sum_lidstone(lambda_v, training_stat, num_all_events):
     """
     Debug the model to verify it sums all the probabilities up to 1
     :param lambda_v: the best lambda
     :param training_stat: training_stat
-    :param validation_stat: validation_stat
-    :return: it halt the program uf fails
+    :param num_all_events: number of all events in training
+    :return: it halt the program if fails
     """
-    p_unseen = lidstone("unseen-word", training_stat, lambda_v)
-    n_0, _ = get_unseen_held_out(validation_stat, training_stat)
-    s = p_unseen * n_0
+    p_unseen = lidstone("unseen-word", training_stat, lambda_v, num_all_events)
+    s = p_unseen * (VOCAB_SIZE - len(training_stat))
     for word in training_stat:
         if training_stat[word] != 0:
-            s += lidstone(word, training_stat, lambda_v)
-    print(s)
-    assert round(s, 4) == 1
+            s += lidstone(word, training_stat, lambda_v, num_all_events)
+
+    print(f"sum lidstone: {s}")
 
 
 def create_rev_stat(set_stat):
@@ -202,13 +215,13 @@ def get_unseen_held_out(h_stat, t_stat):
     """
     Calculate the probability estimation for unseen word using Held out
     """
-    unseen_sum = 0  # T_0
-    unseen_events = 0  # N_0
+    # t_0
+    unseen_sum = 0
     for w_h in h_stat:
         if t_stat[w_h] == 0:
             unseen_sum += h_stat[w_h]
-            unseen_events += 1
-    return unseen_events, unseen_sum
+
+    return unseen_sum
 
 
 def held_out_estimation(word, h_stat, h_size, t_stat, t_reverse_stat):
@@ -217,7 +230,8 @@ def held_out_estimation(word, h_stat, h_size, t_stat, t_reverse_stat):
     """
     r = t_stat[word]
     if r == 0:
-        n_r, t_r = get_unseen_held_out(h_stat, t_stat)
+        n_r = VOCAB_SIZE - len(t_stat)
+        t_r = get_unseen_held_out(h_stat, t_stat)
     else:
         n_r = len(t_reverse_stat[r])
         t_r = get_tr(h_stat, t_reverse_stat, r)
@@ -226,50 +240,49 @@ def held_out_estimation(word, h_stat, h_size, t_stat, t_reverse_stat):
 
 
 def part4(dev, input_word, res):
-    T = dev[:int(0.5 * len(dev))]
-    T_stat = vocab_stt(T)
+    t = dev[:int(0.5 * len(dev))]
+    t_stat = vocab_stt(t)
 
-    H = dev[int(0.5 * len(dev)):]
-    H_stat = vocab_stt(H)
+    h = dev[int(0.5 * len(dev)):]
+    h_stat = vocab_stt(h)
 
     # Number of events in the Held-Out sets
-    o21 = total_event_num(T_stat)
-    H_size = total_event_num(H_stat)
-    o22 = H_size
+    o21 = total_event_num(t_stat)
+    h_size = total_event_num(h_stat)
+    o22 = h_size
     res.append(printify(21, o21))
     res.append(printify(22, o22))
 
     # Held out estimation
-    T_reverse_stat = create_rev_stat(T_stat)
-    o23 = held_out_estimation(input_word, H_stat, H_size, T_stat, T_reverse_stat)
-    o24 = held_out_estimation("unseen-word", H_stat, H_size, T_stat, T_reverse_stat)
+    t_reverse_stat = create_rev_stat(t_stat)
+    o23 = held_out_estimation(input_word, h_stat, h_size, t_stat, t_reverse_stat)
+    o24 = held_out_estimation("unseen-word", h_stat, h_size, t_stat, t_reverse_stat)
     res.append(printify(23, o23))
     res.append(printify(24, o24))
 
     if DEBUG:
-        sum_held_out(H_size, H_stat, T_reverse_stat, T_stat, o24)
+        sum_held_out(h_size, h_stat, t_reverse_stat, t_stat, o24)
 
-    return H_stat, H_size, T_stat, T_reverse_stat
+    return h_stat, h_size, t_stat, t_reverse_stat
 
 
-def sum_held_out(H_size, H_stat, T_reverse_stat, T_stat, unseen_ord_prob):
+def sum_held_out(h_size, h_stat, t_reverse_stat, t_stat, unseen_ord_prob):
     """
     Debug the model to verify it sums all the probabilities up to 1
     :return: it halt the program uf fails
     """
-    n_0, t_0 = get_unseen_held_out(H_stat, T_stat)
-    s = unseen_ord_prob * n_0
-    for word in T_stat:
-        if T_stat[word] != 0:
-            s += held_out_estimation(word, H_stat, H_size, T_stat, T_reverse_stat)
-    print(f"s: {s}")
-    assert round(s, 10) == 1
+    s = unseen_ord_prob * (VOCAB_SIZE - len(t_stat))
+    for word in t_stat:
+        if t_stat[word] != 0:
+            s += held_out_estimation(word, h_stat, h_size, t_stat, t_reverse_stat)
+
+    print(f"sum held out: {s}")
 
 
-def get_tr(H_stat, T_reverse_stat, r):
+def get_tr(h_stat, t_reverse_stat, r):
     t_r = 0
-    for w in T_reverse_stat[r]:
-        t_r += H_stat[w]
+    for w in t_reverse_stat[r]:
+        t_r += h_stat[w]
     return t_r
 
 
@@ -278,9 +291,9 @@ def print_output(output_f, res):
         f.writelines(res)
 
 
-def stringify_table(f_lambda, f_h, N_T_R, T_R):
+def stringify_table(f_lambda, f_h, n_t_r, t_r):
     res = "\n"
-    for r, (a,b,c,d) in enumerate(zip(f_lambda, f_h, N_T_R, T_R)):
+    for r, (a,b,c,d) in enumerate(zip(f_lambda, f_h, n_t_r, t_r)):
         res += f"{r}\t{a}\t{b}\t{c}\t{d}\n"
     return res
 
@@ -296,8 +309,8 @@ def main():
     dev_stat = vocab_stt(dev)
 
     part2(dev_stat, res)
-    best_lambda = part3(dev, input_word, res)
-    H_stat, H_size, T_stat, T_reverse_stat = part4(dev, input_word, res)
+    best_lambda, training_stat, num_all_events = part3(dev, input_word, res)
+    h_stat, h_size, t_stat, t_reverse_stat = part4(dev, input_word, res)
 
     # part 6 - Evaluation on test set
     test = parse_file(test_f)
@@ -308,8 +321,8 @@ def main():
     res.append(printify(25, o25))
 
     # perplexity on test
-    o26 = perplexity_lidstone(test_stat, best_lambda)
-    o27 = perplexity_held_out(H_stat, H_size, test_stat)
+    o26 = perplexity_lidstone(test_stat, training_stat, best_lambda, num_all_events)
+    o27 = perplexity_held_out(h_stat, h_size, test_stat, t_stat)
     res.append(printify(26, o26))
     res.append(printify(27, o27))
 
@@ -319,28 +332,28 @@ def main():
     res.append(printify(28, o28))
 
     # Creating table
-    create_r_table(H_stat, T_reverse_stat, T_stat, res)
+    create_r_table(h_stat, t_reverse_stat, t_stat, res)
 
     # And.......DONE!
     print_output(output_f, res)
 
 
-def create_r_table(H_stat, T_reverse_stat, T_stat, res):
-    N_T_0, T_R_0 = get_unseen_held_out(H_stat, T_stat)
-    N_T_R = [N_T_0]
-    T_R = [T_R_0]
+def create_r_table(h_stat, t_reverse_stat, t_stat, res):
+    t_r_0 = get_unseen_held_out(h_stat, t_stat)
+    n_t_r = [VOCAB_SIZE - len(t_stat)]
+    t_r = [t_r_0]
+
     for r in range(1, 10):
-        n_r = len(T_reverse_stat[r])
-        N_T_R.append(n_r)
-        t_r = get_tr(H_stat, T_reverse_stat, r)
-        T_R.append(t_r)
+        n_r = len(t_reverse_stat[r])
+        n_t_r.append(round(n_r, 5))
+        t_r.append(round(get_tr(h_stat, t_reverse_stat, r), 5))
+
     # TODO fix f_lambda, f_h
     f_lambda = [111] * 10
     f_h = [777] * 10
-    o29 = stringify_table(f_lambda, f_h, N_T_R, T_R)
+    o29 = stringify_table(f_lambda, f_h, n_t_r, t_r)
     res.append(printify(29, o29))
 
 
 if __name__ == '__main__':
-    DEBUG = False
     main()
